cc1a27312c029639a5cfb0fe85f43cf1db0c0480
qttq23 <1753102@student.hcmus.edu.vn>
2020-02-21 23:40:52 +0700

    feature 'list files and folders in a directory' - write list files and folders function - write config class to use config file to set up statup folder and visualize filename - try use git ignore file - write unit test and reconstruct folder structure

---
+++ dirx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+using System.Json;
+
+namespace dirx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // config file
+            Config.Init();
+            string startFolder = Config.json["startFolder"];
+
+            // show list
+            Console.WriteLine($"files and folders at {startFolder}:");
+            var list = FileDirInfo.List(startFolder);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
